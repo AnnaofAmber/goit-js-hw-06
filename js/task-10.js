1211ed7b462043b3eb15fab5d1d089b826386bf5
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-07-10 19:58:58 +0300

    невиконане 10 завдання

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,56 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const input = document.querySelector('input')
+
+const btnCreate = document.querySelector('button[data-create]')
+const boxes = document.querySelector('#boxes')
+let box
+function createBoxes(amount) {
+  const arr = []
+  amount = Number(input.value)
+  for (let i = amount; i>=1; i -= 1){
+    const number = i
+    arr.push(number)
+  }
+
+  arr.forEach(function (item, index) {
+    box = document.createElement('div')
+    boxes.append(box)
+    box.style.width = `30px`;
+    box.style.height = `30px`;
+    box.style.backgroundColor = getRandomHexColor()
+    box.classList.add('box')
+      
+    
+  //   for (let i = 0; i <= 3; i += 1) {
+  //   console.log(i);
+  //   let val = 30;
+  //   val[i] += 10
+  //   console.log(val);
+  //   console.log(i);
+  //     box.style.width = `${val}px`;
+  //     box.style.height = `${val}px`;
+  //   }
+    
+  })
+  
+}
+
+btnCreate.addEventListener('click', createBoxes)
+
+
+
+const btnDestroy = document.querySelector('button[data-destroy]')
+
+function destroyBoxes() {
+
+  const removeBox = document.querySelector(".box");
+  removeBox.remove()
+
+  }
+btnDestroy.addEventListener('click', destroyBoxes)
+
+ 
