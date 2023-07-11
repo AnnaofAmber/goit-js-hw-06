3cc457980c10af0545326ebca15a3040d41602d5
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-07-11 14:43:17 +0300

    додає виправлене 6 завдання

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,14 @@
 const input = document.querySelector('#validation-input')
 
-input.addEventListener('blur', () => {
+input.addEventListener('blur', (event) => {
+    if ('blur') {
         if (input.value.length === Number(input.getAttribute('data-length'))) {
-        input.classList.add('valid')
+            input.classList.add('valid')
+            input.classList.remove('invalid')
         } else {
+            input.classList.remove('valid')
             input.classList.add('invalid')
         }
+    }
 })
 
